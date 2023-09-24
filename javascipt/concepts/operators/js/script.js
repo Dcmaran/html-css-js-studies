5bfd407480fb0c860dbd2af461db88d27606dc95
Dcmaran <diogocmr2002@gmail.com>
2023-09-24 13:44:22 -0300

    .

---
+++ javascipt/concepts/operators/js/script.js
@@ -0,0 +1,76 @@
+// 1 - Number
+
+console.log(typeof 2);
+console.log(typeof -500);
+console.log(typeof 5.2);
+
+// 2 - Operators +, -, /, *
+
+console.log(2 + 2);
+console.log(8 + (13 * 14)/4);
+
+// 3 - Special Numbers
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+
+console.log(12 * "asd");
+
+// 4 - Strings
+
+console.log(typeof "A text");
+console.log(typeof `More text`);
+
+// 5 - Special string symbols
+
+console.log("Testing \nline break")
+console.log("Testing \t tab")
+
+// 6 - Concatenation
+
+console.log("Hi," + " how" + " are you?");
+
+// 7 Interpolation
+
+const name = "Pedro"
+
+console.log(`My name is ${name}`)
+
+// 8 - Boolean
+
+console.log(true);
+console.log(5 > 20);
+console.log(false);
+console.log(typeof (5 > 20));
+
+// 9 - Comparing
+
+console.log(10 == 10);
+console.log(10 != 9);
+console.log(10 == 9);
+
+// 10 - Strict equality
+
+console.log(9 == "9");
+console.log(9 === "9");
+
+console.log(9 != "9");
+console.log(9 !== "9");
+
+// 11 - Logical operators && - || - !
+
+// 12 - Empty Values
+
+console.log(typeof null, typeof undefined);
+
+// 13 - Type conversion
+
+console.log(5 * null)
+
+console.log("test" * "hello")
+
+console.log("10" - 1);
+
+console.log("10" + 1);
+
