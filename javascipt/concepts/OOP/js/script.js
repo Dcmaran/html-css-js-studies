a8f9e6e455366a2100bdce4ca660e7953825be0e
Dcmaran <diogocmr2002@gmail.com>
2023-10-05 21:35:54 -0300

    .

--- javascipt/concepts/OOP/js/script.js
+++ javascipt/concepts/OOP/js/script.js
@@ -1,14 +1,28 @@
-const dog = {
-    name: "Bebel",
-    bark: function () {
-        console.log("Au Au");
-    }
-};
+// const dog = {
+//     name: "Bebel",
+//     bark: function () {
+//         console.log("Au Au");
+//     }
+// };
 
-dog.bark();
+// dog.bark();
 
 // 1 - Prototype
 
 const text = "asd";
 
 console.log(Object.getPrototypeOf(text));
+
+// 2 - Basic Classes
+
+const dog = {
+    race: null,
+    paws: 4,
+};
+
+const germanShepherd = Object.create(dog);
+
+germanShepherd.race = "germanShepherd";
+
+console.log(germanShepherd);
+console.log(germanShepherd.paws);
